6aac799871897e7029342bb4bacdeadc009cea66
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-10-06 11:09:45 +0530

    New commit

--- src/main/java/com/DemoProjec1/generic/BaseTest.java
+++ src/main/java/com/DemoProjec1/generic/BaseTest.java
@@ -8,9 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest{
 
-	public static void main(String[] args) {
 	
-	}
 	public WebDriver driver;
 	@BeforeClass
 	public void configBC()
