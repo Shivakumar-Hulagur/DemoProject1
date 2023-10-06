aab4862adf3152fb4b329435cbe02ed1caa2fd8d
sumukhgs <sumukh.g.s.30@gmail.com>
2023-10-06 11:05:29 +0530

    Merge branch 'master' of https://github.com/Shivakumar-Hulagur/DemoProject1.git

--- src/main/java/com/DemoProjec1/generic/BaseTest.java
+++ src/main/java/com/DemoProjec1/generic/BaseTest.java
@@ -2,10 +2,6 @@ package com.DemoProjec1.generic;
 
 import java.time.Duration;
 import org.testng.annotations.*;
-
-import com.DemoProject1.pom.RegisterPage;
-
-import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
